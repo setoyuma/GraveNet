ee0fe8bcc90426f5d5b5b704a58095db125e79be
seto yuma <setoichi.dev@gmail.com>
2023-04-09 02:33:21 -0700

    started homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className=" flex justify-center text-6xl text-teal-500">
+      <h1 className=" bg-gradient-to-tr to-neutral-900 from-teal-900 animate-gradient-xy rounded-lg absolute text-center p-1.5">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        "gradient-x": "gradient-x 15s ease infinite",
+        "gradient-y": "gradient-y 15s ease infinite",
+        "gradient-xy": "gradient-xy 20s ease infinite",
+      },
+      keyframes: {
+        dropShadow: {
+          glow: [
+            "0 0px 20px rgba(255,255, 255, 0.35)",
+            "0 0px 65px rgba(255, 255,255, 0.2)",
+          ],
+        },
+        "gradient-y": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "center top",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "center center",
+          },
+        },
+        "gradient-x": {
+          "0%, 100%": {
+            "background-size": "200% 200%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+        "gradient-xy": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position": "left center",
+          },
+          "50%": {
+            "background-size": "200% 200%",
+            "background-position": "right center",
+          },
+        },
+      }
+    },
+  },
+  plugins: [],
+  
+}
